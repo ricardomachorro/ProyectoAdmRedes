b3a42169d5a777dc9e95bc49ee5efc23efd717e5
ricardomachorro <ricardoalbeto.machorrovences@gmail.com>
2021-10-21 16:48:49 -0500

    Actualizacion Crear nuevo elemento de red RIP

--- codigoredes/conexionSSH.py
+++ codigoredes/conexionSSH.py
@@ -1,5 +1,8 @@
 import paramiko,getpass,time
-ip="10.0.1.254"
+
+
+username="cisco"
+password="cisco"
 max_buffer=65535
 
 def clear_buffer(connection):
@@ -7,16 +10,20 @@ def clear_buffer(connection):
         return connection.recv(max_buffer)
 
 def crear_rutaRIP(nombre_ruta):
+    contadorLineaArchivo=0
     connection=paramiko.SSHClient()
     connection.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     connection.connect(ip,username=username,password=password,look_for_keys=False,allow_agent=False)
     new_connection=connection.invoke_shell()
     output=clear_buffer(new_connection)
     time.sleep(2)
-    with open("comandos.txt","r") as file:
-    for line in file:
-        new_connection.send(line,rstrip()+"\n")
-        time.sleep(2)
-        output=new_connection.recv(max_buffer)
-        output=clear_buffer(new_connection)
+    with open("nuevaRedRIP.txt","r") as file:
+        for line in file:
+            contadorLineaArchivo+=1
+            if contadorLineaArchivo==4:
+                line=line.format(direccion=nombre_ruta)
+            new_connection.send(line,rstrip()+"\n")
+            time.sleep(2)
+            output=new_connection.recv(max_buffer)
+            output=clear_buffer(new_connection)
     new_connection.close()
